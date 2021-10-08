ca26a9882cfa46ca3c5e3d32192d90606eba3101
asa1997 <arun.suresh@wipro.com>
2021-10-08 17:04:55 +0530

    Added details of prometheus & wordpress

---
+++ assets/data/OSSP-master.js
@@ -0,0 +1,180 @@
+modelDataAvailable({
+    "total_count": 1,
+    "incomplete_results": false,
+    "items":
+[  
+    {
+        "id": 50,      
+        "bes_tracking_id": 50,
+        "name": "prometheus",
+        "full_name": "Be-Secure/prometheus",
+        "description": "The Prometheus monitoring system and time series database.",
+        "watchers_count": 0,
+        "forks_count": 0,
+        "stargazers_count": 0,
+        "created_at": "2011-11-30T15:30:13Z",
+        "updated_at": "2021-09-03T21:15:41Z",
+        "pushed_at": "2021-09-04T04:41:48Z",
+        "git_url": "git://github.com/magento/magento2.git",
+        "clone_url": "https://github.com/magento/magento2.git",
+        "html_url": "https://github.com/Be-Secure",
+        "homepage": "http://www.magento.com",
+        "owner": {
+            "login": "Be-Secure",
+            "id": 44028837,
+            "avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+            "html_url": "https://github.com/Be-Secure",
+          },
+        "cvedetails": {     
+        
+            "count": 1,                                             
+            "bes_cve_details_id": "",                      
+            "cvedetails_product_id": "61503",                      
+            "cvedetails_vendor_id": "20905"
+        }, 
+        "project_repos": {
+            "main_github_url": "https://github.com/prometheus/prometheus",
+            "main_bes_url": "https://github.com/Be-Secure/prometheus",
+            "all_projects": {                                                                         
+                "prometheus": "https://github.com/prometheus/prometheus",
+                // "sub_project_xxx":""
+            },
+            "all_bes_repos": [                                                                  
+                {
+                    "id": 391939220,
+                    "name": "prometheus",
+                    "url": "https://github.com/Be-Secure/prometheus"
+                }
+                
+            ]
+        },
+        "license": {
+            "key": "apache-2.0",
+            "name": "Apache License 2.0",
+            "spdx_id": "Apache-2.0",
+            "url": "https://api.github.com/licenses/apache-2.0",
+            "node_id": "MDc6TGljZW5zZTI="
+          },   
+        "language": [
+
+            "Go",
+            "TypeScript",
+            "JavaScript",
+            "HTML",
+            "Yacc",
+            "SCSS",
+            "CSS",
+            "Shell",
+            "Lex",
+            "Makefile",
+            "Dockerfile"
+
+        ],
+        "tags": {
+            "IND": [
+                "ALL"
+            ],
+            "SD": [
+                "SD-SOC"
+            ],
+            "COM": {
+                "foundation": "CNCF"
+            },
+            "TD-U": [
+                "TD-U-DAA"
+            ]
+
+        }
+            
+          
+    },
+    {
+        "id": 72,      
+        "bes_tracking_id": 72,
+        "name": "WordPress",
+        "full_name": "Be-Secure/Wordpress",
+        "description": "WordPress is a free and open-source content management system written in PHP and paired with a MySQL or MariaDB database. Features include a plugin architecture and a template system, referred to within WordPress as Themes.",
+        "stargazers_count": 0,
+        "watchers_count": 0,
+        "forks_count": 0,
+        "created_at": "2021-09-04T08:43:07Z",
+        "updated_at": "2021-09-04T08:43:09Z",
+        "pushed_at": "2021-09-04T00:40:07Z",
+        "git_url": "git://github.com/Be-Secure/WordPress.git",
+        "clone_url": "https://github.com/Be-Secure/WordPress.git",
+        "html_url": "https://github.com/Be-Secure",
+        "homepage": "https://wordpress.org",
+        "owner": {
+            "login": "Be-Secure",
+            "id": 44028837,
+            "avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+            "html_url": "https://github.com/Be-Secure",
+          },
+        "cvedetails": {     
+        
+            "count": 7,                                             
+            "bes_cve_details_id": "",                      
+            "cvedetails_product_id": "4096",                      
+            "cvedetails_vendor_id": "2337"
+        }, 
+        "project_repos": {
+            "main_github_url": "https://github.com/WordPress/WordPress",
+            "main_bes_url": "https://github.com/Be-Secure/WordPress",
+            "all_projects": [
+                {
+                    "name": "Wordpress",
+                    "url": "https://github.com/WordPress/WordPress",
+                }
+
+            ],                                                                      
+                
+                // "sub_project_xxx":""
+            "all_bes_repos": [                                                                  
+                {
+                    "id": 391939220,
+                    "name": "prometheus",
+                    "url": "https://github.com/Be-Secure/WordPress"
+                }
+                
+            ]
+        },
+        "license": {
+            "key": "other",
+            "name": "Other",
+            "spdx_id": "NOASSERTION",
+            "url": null,
+            "node_id": "MDc6TGljZW5zZTA="
+          },
+        "language": [
+
+        
+            "PHP",
+            "JavaScript",
+            "CSS",
+            "SCSS"
+        
+
+        ],
+        "tags": {
+            "IND": [
+                "ALL"
+            ],
+            "SD": [
+                ""
+            ],
+            "COM": {
+                "Community": ""
+            },
+            "TD-U": [
+                ""
+            ]
+
+        }
+            
+          
+    },
+    
+
+  
+]
+});
