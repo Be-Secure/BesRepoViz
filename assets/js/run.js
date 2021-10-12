24e587e34f188788f57541df89948aaf73e21489
asa1997 <arun.suresh@wipro.com>
2021-10-12 15:24:50 +0530

    Updated

--- assets/js/run.js
+++ assets/js/run.js
@@ -80,157 +80,228 @@ function Headers(list, selector) {
 
 var chart_vulntypeschart;
 function vulntypeschart(data){
-  for (var i in data) {
-    if (data[i].Year == "Total") {
-      var data_array = [
-        ["XSS",data[i].XSS],["Http Response Splitting",data[i].Http_Response_Splitting],["Code Execution",data[i].Code_Execution],["Sql Injection",data[i].Sql_Injection],["Gain Information",data[i].Gain_Information],["Denial of Service",data[i].DoS],["Directory Traversal",data[i].Directory_Traversal],["Bypass Something",data[i].Bypass_something],["CSRF",data[i].CSRF],["Gain Privilege",data[i].Gain_Privileges],["File Inclusion",data[i].File_Inclusion],];
+  // for (var i in data) {
+  //   if (data[i].Year == "Total") {
+  //     var data_array = [
+  //       ["XSS",data[i].XSS],["Http Response Splitting",data[i].Http_Response_Splitting],["Code Execution",data[i].Code_Execution],["Sql Injection",data[i].Sql_Injection],["Gain Information",data[i].Gain_Information],["Denial of Service",data[i].DoS],["Directory Traversal",data[i].Directory_Traversal],["Bypass Something",data[i].Bypass_something],["CSRF",data[i].CSRF],["Gain Privilege",data[i].Gain_Privileges],["File Inclusion",data[i].File_Inclusion],];
       
-    }
-  }
+  //   }
+  // }
+    var data_array = [["XSS",data[data.length-2].XSS],["Http Response Splitting",data[data.length-2].Http_Response_Splitting],["Code Execution",data[data.length-2].Code_Execution],["Sql Injection",data[data.length-2].Sql_Injection],["Gain Information",data[data.length-2].Gain_Information],["Denial of Service",data[data.length-2].DoS],["Directory Traversal",data[data.length-2].Directory_Traversal],["Bypass Something",data[data.length-2].Bypass_something],["CSRF",data[data.length-2].CSRF],["Gain Privilege",data[data.length-2].Gain_Privileges],["File Inclusion",data[data.length-2].File_Inclusion],];
+    console.log("data_array");
+    console.log(data_array);    
     chart_vulntypeschart = jQuery.jqplot ("vulntypeschart", [data_array],
 		{
+      grid: {
+        drawBorder: false, 
+        drawGridlines: false,
+        background: '#ffffff',
+        shadow:false
+    },
+      title: "% of vulnerabilities",
 		  seriesDefaults: {
 			// Make this a pie chart.
 			renderer: jQuery.jqplot.PieRenderer,
-			rendererOptions: {
-			  showDataLabels: true
-			}
+      trendline:{ show:false }, 
+      rendererOptions: { 
+        showDataLabels: true}
 		  },
-		  legend: { show:true,
+		  legend: { 
+        show:true,
 			  location: 'e',
 			  placement: 'outsideGrid'
 		  },
-		  seriesColors: [
-			  '#999','#DB045B','#E04807',
-			  '#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE','#DB843D', '#92A8CD', '#A47D7C', '#B5CA92','#FF9655','#24CBE5'
-		  ]
+		  // seriesColors: [
+			//   '#999','#DB045B','#E04807',
+			//   '#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE','#DB843D', '#92A8CD', '#A47D7C', '#B5CA92','#FF9655','#24CBE5'
+		  // ]
 		}
 
 	  );
 }
-var chart_vulnsbytypeandyearchart;
 
 function vulnsbytypeandyearchart(data){
-  console.log("inside line chart");
-  var vuln=[];
-	var vulntypesarray=['No_of_Vulnerabilities', 'XSS', 'Http_Response_Splitting', 'Code_Execution', 'Sql_Injection', 'Gain_Information', 'DoS', 'Directory_Traversal', 'Bypass_Something', 'CSRF', 'No_of_exploits', 'Gain_Privileges', 'File_Inclusion'];
+  var chart_vulnsbytypeandyearchart;
+	var vuln=[];
+  var total_array=[];
+  var xss_array=[];
+  var hrs_array=[];
+  var ec_array=[];
+  var sqli_array=[];
+  var gi_array=[];
+  var dos_array=[];
+  var dt_arraay=[];
+  var bs_array=[];
+  var csrf_array=[];
+  var exploits_array=[];
+  var gp_array=[];
+  var fi_array=[];
+	var vulntypesarray=[];
+					vulntypesarray.push("Total");
+
+              for (let i = 0; i < data.length-2; i++) {
 
 
-  for (let i = 0; i < data.length-1; i++) {
-   vuln.push([[data[i].Year,data[i].No_of_Vulnerabilities]])
-    
-  }
-  for (let i = 0; i < data.length-1; i++) {
-    vuln.push([[data[i].Year,data[i].XSS]])
-     
-  }
-  for (let i = 0; i < data.length-1; i++) {
-    vuln.push([[data[i].Year,data[i].Http_Response_Splitting]])
-     
-  }
-  for (let i = 0; i < data.length-1; i++) {
-    vuln.push([[data[i].Year,data[i].Code_Execution]])
-     
-  }
-  for (let i = 0; i < data.length-1; i++) {
-    vuln.push([[data[i].Year,data[i].Sql_Injection]])
-    
-  }
-  for (let i = 0; i < data.length-1; i++) {
-    vuln.push([[data[i].Year,data[i].Gain_Information]])
-    
-  }
+                 total_array.push([data[i].Year,data[i].No_of_Vulnerabilities]);
 
-  for (let i = 0; i < data.length-1; i++) {
-  vuln.push([[data[i].Year,data[i].DoS]])
-    
-  }
-  for (let i = 0; i < data.length-1; i++) {
-    vuln.push([[data[i].Year,data[i].Directory_Traversal]])
-    
-  }
-  for (let i = 0; i < data.length-1; i++) {
-    vuln.push([[data[i].Year,data[i].Bypass_something]])
-    
-  }
-  for (let i = 0; i < data.length-1; i++) {
-    vuln.push([[data[i].Year,data[i].CSRF]])
-    
-  }
-  for (let i = 0; i < data.length-1; i++) {
-    vuln.push([[data[i].Year,data[i].No_of_exploits]])
+              }
+
+                
+              
+							vulntypesarray.push("XSS");
+
+        for (let i = 0; i < data.length-2; i++) {
+
+         
+          xss_array.push([data[i].Year,data[i].XSS]);
+
+        }
+							vulntypesarray.push("Http Response Splitting");
+              for (let i = 0; i < data.length-2; i++) {
+
+               
+                hrs_array.push([data[i].Year,data[i].Http_Response_Splitting]);
       
-  }
-  for (let i = 0; i < data.length-1; i++) {
-    vuln.push([[data[i].Year,data[i].Gain_Privileges]])
-      
-  }
-  for (let i = 0; i < data.length-1; i++) {
-    vuln.push([[data[i].Year,data[i].File_Inclusion]])
-      
-  }
+              }
+				//},
+							vulntypesarray.push("Execute Code");
+              for (let i = 0; i < data.length-2; i++) {
 
-  chart_vulnsbytypeandyearchart = $.jqplot ('vulnsbytypeandyearchart', vuln, {
-    seriesColors: [
-    '#69c','#333','#DB045B','#E04807',
-    '#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE','#DB843D', '#92A8CD', '#A47D7C', '#B5CA92','#FF9655','#24CBE5'
-    ],
-  seriesDefaults: {
-  },
-  // series:[
-  //   {show:true, label:"No_of_Vulnerabilities"},{show:false, label:"XSS"},{show:false, label:"Http_Response_Splitting"},{show:true, label:"Code_Execution"},{show:false, label:"Sql_Injection"},{show:false, label:"Gain_Information"},{show:false, label:"DoS"},{show:false, label:"Directory_Traversal"},{show:false, label:"Bypass_Something"},{show:false, label:"CSRF"},{show:true, label:"No_of_exploits"},{show:false, label:"Gain_Privileges"},{show:false, label:"File_Inclusion"},		],
-  title:{
-    text: 'Vulnerabilities by type & year',
-    fontFamily: '"Lucida Grande", "Lucida Sans Unicode", Verdana, Arial, Helvetica, sans-serif', // default font
-    fontSize: '12px',
-    color: '#3E576F'
-  },
-    axesDefaults: {
-      labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
-  borderColor: '#4572A7',
-    },
-    axes: {
-      xaxis: {
-        label: "Years",
-    color: '#3E576F',
-        pad: 0,
-    min:1999,
-    tickInterval: 1
+               
+               ec_array.push([data[i].Year,data[i].Code_Execution]);
+      
+              }
+							vulntypesarray.push("Sql Injection");
+              for (let i = 0; i < data.length-2; i++) {
+
+               
+                sqli_array.push([data[i].Year,data[i].Sql_Injection]);
+      
+              }
+							vulntypesarray.push("Gain Information");
+              for (let i = 0; i < data.length-2; i++) {
+
+               
+                gi_array.push([data[i].Year,data[i].Gain_Information]);
+      
+              }
+							vulntypesarray.push("Denial of Service");
+              for (let i = 0; i < data.length-2; i++) {
+
+               
+                dos_array.push([data[i].Year,data[i].DoS]);
+      
+              }
+							vulntypesarray.push("Directory Traversal");
+              for (let i = 0; i < data.length-2; i++) {
+
+               
+                dt_arraay.push([data[i].Year,data[i].Directory_Traversal]);
+      
+              }
+							vulntypesarray.push("Bypass Something");
+              for (let i = 0; i < data.length-2; i++) {
+
+               
+                bs_array.push([data[i].Year,data[i].Bypass_something]);
+      
+              }
+							vulntypesarray.push("CSRF");
+              for (let i = 0; i < data.length-2; i++) {
+
+               
+                csrf_array.push([data[i].Year,data[i].CSRF]);
+      
+              }
+							vulntypesarray.push("Exploits");
+              for (let i = 0; i < data.length-2; i++) {
+
+               
+                 exploits_array.push([data[i].Year,data[i].No_of_exploits]);
+      
+              }
+							vulntypesarray.push("Gain Privilege");
+              for (let i = 0; i < data.length-2; i++) {
+
+               
+                 gp_array.push([data[i].Year,data[i].Gain_Privileges]);
+      
+              }
+							vulntypesarray.push("File Inclusion");
+              for (let i = 0; i < data.length-2; i++) {
+
+               
+                fi_array.push([data[i].Year,data[i].File_Inclusion]);
+      
+              }
+				//},
+        vuln.push(total_array,xss_array,hrs_array,ec_array,sqli_array,gi_array,dos_array,dt_arraay,bs_array,csrf_array,exploits_array,gp_array,fi_array);
+    console.log("vuln:");
+		console.log(vuln);	
+    console.log("vulntypesarray:");
+    console.log(vulntypesarray);
+	chart_vulnsbytypeandyearchart = $.jqplot ('vulnsbytypeandyearchart', vuln, {
+      seriesColors: [
+		  '#69c','#333','#DB045B','#E04807',
+		  '#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE','#DB843D', '#92A8CD', '#A47D7C', '#B5CA92','#FF9655','#24CBE5'
+		  ],
+      total: "Vulnerabilities by Type and Year",
+		seriesDefaults: {
+		},
+		series:[
+			{show:true, label:"Total"},{show:false, label:"XSS"},{show:false, label:"Http Response Splitting"},{show:true, label:"Execute Code"},{show:false, label:"Sql Injection"},{show:false, label:"Gain Information"},{show:false, label:"Denial of Service"},{show:false, label:"Directory Traversal"},{show:false, label:"Bypass Something"},{show:false, label:"CSRF"},{show:true, label:"Exploits"},{show:false, label:"Gain Privilege"},{show:false, label:"File Inclusion"},		],
+	  title:{
+			text: 'Vulnerabilities by type & year',
+			fontFamily: '"Lucida Grande", "Lucida Sans Unicode", Verdana, Arial, Helvetica, sans-serif', // default font
+			fontSize: '12px',
+			color: '#3E576F'
+	  },
+      axesDefaults: {
+        labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
+		borderColor: '#4572A7',
       },
-      yaxis: {
-        label: "# Of Vulns",
-    numberTicks : 5,
-    min:0 ,
-    color: '#3E576F'
-      }
-    },
-  legend: {
-    show:true,
-    location: 's',
-    renderer: $.jqplot.EnhancedLegendRenderer,
-    placement: 'outsideGrid',
-    labels: vulntypesarray,
-    rendererOptions:{
-      seriesToggle: true,
-      seriesToggleReplot: {resetAxes:['yaxis']},
-      numberRows: 5,
-      numberColumns: 5
-    },
-    border:'1px solid #909090',
-    fontFamily: '"Lucida Grande", "Lucida Sans Unicode", Verdana, Arial, Helvetica, sans-serif', // default font
-    fontSize: '11px'
-  },
-  highlighter: {
-    show: true,
-    sizeAdjust: 12,
-    fadeTooltip: false,
-    formatString: "%d : %d",
-    bringSeriesToFront:true,
-    lineWidthAdjust:10
-  }
-  });
+      axes: {
+        xaxis: {
+          label: "Years",
+		  color: '#3E576F',
+          pad: 0,
+		  min:1999,
+		  tickInterval: 1
+        },
+        yaxis: {
+          label: "# Of Vulns",
+		  numberTicks : 5,
+		  min:0 ,
+		  color: '#3E576F'
+        }
+      },
+	  legend: {
+		  show:true,
+		  location: 's',
+		  renderer: $.jqplot.EnhancedLegendRenderer,
+		  placement: 'outsideGrid',
+		  labels: vulntypesarray,
+		  rendererOptions:{
+			  seriesToggle: true,
+			  seriesToggleReplot: {resetAxes:['yaxis']},
+			  numberRows: 5,
+			  numberColumns: 5
+		  },
+		  border:'1px solid #909090',
+		  fontFamily: '"Lucida Grande", "Lucida Sans Unicode", Verdana, Arial, Helvetica, sans-serif', // default font
+		  fontSize: '11px'
+	  },
+	  highlighter: {
+		  show: true,
+		  sizeAdjust: 12,
+		  fadeTooltip: false,
+		  formatString: "%d : %d",
+		  bringSeriesToFront:true,
+		  lineWidthAdjust:10
+	  }
+    });
 
-}
+	}
 
 function bar_chart_by_year(data)
 {
@@ -255,6 +326,7 @@ function bar_chart_by_year(data)
     plot1 = $.jqplot('bar_chart_vuln_by_year', [vuln], {
         // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
         animate: !$.jqplot.use_excanvas,
+        title: "Vulnerabilities by Year",
         seriesDefaults:{
             renderer:$.jqplot.BarRenderer,
             rendererOptions: {
@@ -301,6 +373,7 @@ function bar_chart_by_type(data){
     plot1 = $.jqplot('bar_chart_vuln_by_type', [vuln], {
         // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
         animate: !$.jqplot.use_excanvas,
+        title: "Vulnerabilities by Type",
         seriesDefaults:{
             renderer:$.jqplot.BarRenderer,
             rendererOptions: {
@@ -363,8 +436,8 @@ function load_data(id,name)
     console.log("inside load data");
     constructTable('#table', data);
     vulntypeschart(data);
+    vulnsbytypeandyearchart(data);
     // console.log(data.data[0].XSS);
-    // vulnsbytypeandyearchart(data);
     bar_chart_by_year(data);
     bar_chart_by_type(data);
     replace_div();
